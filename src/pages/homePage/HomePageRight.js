b8a182150488f0294570570d461c045b799ff1ad
mturkben <mturkben721@gmail.com>
2021-02-13 21:38:26 +0300

    Added Right Side from Home Page

---
+++ src/pages/homePage/HomePageRight.js
@@ -0,0 +1,71 @@
+import React from 'react'
+
+import NoProfileLogo from '../../assets/no_profile_photo.jpg'
+import "../style.css"
+
+const HomePageRight = () => {
+	return (
+		<>
+			<div className="hp-r-center-body d-flex justify-content-between align-items-center flex-row my-2">
+				<div className="left d-flex justify-content-around align-items-center flex-row">
+					<img className="img-fluid rounded-circle mr-2" src={NoProfileLogo} width="35"/>
+					<div className="d-flex justify-content-center align-items-start flex-column w-100">
+						<div>muhammedturkben</div>
+						<span>kaplan_mslm + 1 diğer kişi takip ediyor</span>
+					</div>
+				</div>
+				<div className="right">
+					Takip Et
+				</div>
+			</div><div className="hp-r-center-body d-flex justify-content-between align-items-center flex-row my-2">
+				<div className="left d-flex justify-content-around align-items-center flex-row">
+					<img className="img-fluid rounded-circle mr-2" src={NoProfileLogo} width="35"/>
+					<div className="d-flex justify-content-center align-items-start flex-column w-100">
+						<div>muhammedturkben</div>
+						<span>kaplan_mslm + 1 diğer kişi takip ediyor</span>
+					</div>
+				</div>
+				<div className="right">
+					Takip Et
+				</div>
+			</div><div className="hp-r-center-body d-flex justify-content-between align-items-center flex-row my-2">
+				<div className="left d-flex justify-content-around align-items-center flex-row">
+					<img className="img-fluid rounded-circle mr-2" src={NoProfileLogo} width="35"/>
+					<div className="d-flex justify-content-center align-items-start flex-column w-100">
+						<div>muhammedturkben</div>
+						<span>kaplan_mslm + 1 diğer kişi takip ediyor</span>
+					</div>
+				</div>
+				<div className="right">
+					Takip Et
+				</div>
+			</div>
+			<div className="hp-r-center-body d-flex justify-content-between align-items-center flex-row my-2">
+				<div className="left d-flex justify-content-around align-items-center flex-row">
+					<img className="img-fluid rounded-circle mr-2" src={NoProfileLogo} width="35"/>
+					<div className="d-flex justify-content-center align-items-start flex-column w-100">
+						<div>muhammedturkben</div>
+						<span>kaplan_mslm + 1 diğer kişi takip ediyor</span>
+					</div>
+				</div>
+				<div className="right">
+					Takip Et
+				</div>
+			</div>
+			<div className="hp-r-center-body d-flex justify-content-between align-items-center flex-row my-2">
+				<div className="left d-flex justify-content-around align-items-center flex-row">
+					<img className="img-fluid rounded-circle mr-2" src={NoProfileLogo} width="35"/>
+					<div className="d-flex justify-content-center align-items-start flex-column w-100">
+						<div>muhammedturkben</div>
+						<span>kaplan_mslm + 1 diğer kişi takip ediyor</span>
+					</div>
+				</div>
+				<div className="right">
+					Takip Et
+				</div>
+			</div>
+		</>
+	);
+}
+
+export default HomePageRight
